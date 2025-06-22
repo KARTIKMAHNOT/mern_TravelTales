ec26d8ae20c22f39145587c306176958fd2b1142
Kartik Mahnot <kartikmahnot1@gmail.com>
2025-06-22 15:08:04 +0530

    correct

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://mern-traveltales.onrender.com/api',
   withCredentials: true, // Include cookies in requests
   
 });
